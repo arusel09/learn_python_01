383335e0181a5e1944e062e6c5e10ceeb09fa179
Arunprakash Selvaraj <maverick@Arunprakashs-MacBook-Pro.local>
2018-12-30 18:55:14 +0530

    Trying Numbers& Strings

---
+++ print_number_strings.py
@@ -0,0 +1,30 @@
+# Show my taxes!
+
+my_income = 100
+
+tax_rate = 0.1
+
+my_taxes = my_income*tax_rate
+
+print (my_taxes)
+
+#Cant add string only concatenate is possible
+
+print('2'+'3')
+
+#We can use the multiplication symbol to create repetition!
+letter = 'z'
+print (letter*10)
+
+
+#New Line
+print ("Hello \nWorld 01" )
+print ("\n")
+print ("Hello \nWorld 02" )
+
+#To Test String Methods
+mystring = 'Hello World'
+print (mystring.upper())
+print (mystring.lower())
+print (mystring.split())
+print (mystring.split('o'))
